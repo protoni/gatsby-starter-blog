3f5471dbc51c95d2f73202aa9afcbd1c4d1c02de
Toni Kamula <protoni91@gmail.com>
2020-07-30 21:34:55 +0300

    Fix code block highlighting equal signs issue.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,4 +2,11 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 
-import "prismjs/themes/prism.css"
+//import "prismjs/themes/prism.css"
+
+//require("prismjs/themes/prism-okaidia.css")
+
+// Custom prism-okaidia.css
+require("./src/css/prism-custom.css")
+
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
