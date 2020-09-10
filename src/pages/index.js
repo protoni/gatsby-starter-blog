7f8246c09ec7edc14a44dad3af41038a2a30b03e
Toni Kamula <protoni91@gmail.com>
2020-09-10 22:17:39 +0300

    Added RD350 introduction post.

--- src/pages/index.js
+++ src/pages/index.js
@@ -111,6 +111,22 @@ const BlogIndex = ({ data, location }) => {
          <p style={{fontSize: '0.9em', fontWeight: 'bold'}}>{"Misc"} ({tag.totalCount})</p>
         </div>
       )
+    :tag.fieldValue === "motorcycles" ?
+      (
+        /* bolt.svg ( https://tablericons.com/ ) */
+        <div>
+          <Link to={`/tags/${kebabCase(tag.fieldValue)}/`} style={{ boxShadow: 'none' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-bike" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2196F3" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z"/>
+              <circle cx="5" cy="18" r="3" />
+              <circle cx="19" cy="18" r="3" />
+              <polyline points="12 19 12 15 9 12 14 8 16 11 19 11" />
+              <circle cx="17" cy="5" r="1" />
+            </svg>
+          </Link>
+         <p style={{fontSize: '0.9em', fontWeight: 'bold'}}>{"Motorcycles"} ({tag.totalCount})</p>
+        </div>
+      )
     :tag.fieldValue === "protoni website" ?
       (
         /* bolt.svg ( https://tablericons.com/ ) */
